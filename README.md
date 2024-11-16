f28a46a6838948cffed184d921b8f4688ad7bf48
artemyr <artem.affetta@gmail.com>
2024-11-16 15:30:37 +0300

    подготовка

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
+# Установка
 - php artisan key:generate
 - php artisan migrate
+- php artisan storage:link
+
+в .env FILESYSTEM_DISK установить public при необходимости
+
+# Зависимости
+1. barryvdh/laravel-debugbar - отладка
+> работает когда в .env APP_DEBUG=true и APP_ENV не production
+2. laravel/telescope - просмотр логов
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,36 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Connection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        /**
+         * защита от проблемы ленивой загрузки отношений N+1
+         * когда отношения модели подгружаются автоматически без явного указания
+         * отножения нужно будет указывать явно, например так Post::with('author')->get()
+         */
+        Model::preventLazyLoading(!app()->isProduction());
+        /**
+         * в локальной разработке выдавать ошибку, если пытаемся
+         * записать данные в защищенное поле модели
+         */
+        Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
+        /**
+         *
+         */
+        DB::whenQueryingForLongerThan(500, function (Connection $connection) {
+            // TODO
+        });
     }
 }
