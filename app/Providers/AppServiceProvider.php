4c565f603fda111ababbea32ad0db506ad9b0b51
artemyr <artem.affetta@gmail.com>
2024-11-16 17:59:00 +0300

    логгер

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonInterval;
 use Illuminate\Database\Connection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Http\Kernel;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
@@ -28,10 +30,24 @@ class AppServiceProvider extends ServiceProvider
          */
         Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
         /**
-         *
+         * если запрос в бд слишком долго обрабатывается отправляем лог в телеграм
          */
         DB::whenQueryingForLongerThan(500, function (Connection $connection) {
-            // TODO
+            logger()
+                ->channel('telegram')
+                ->debug('whenQueryingForLongerThan:' . $connection->query()->toSql());
         });
+        /**
+         * если запрос слишком долго отрабатывает, то отправляем лог в телеграмл
+         */
+        $kernel = app(Kernel::class);
+        $kernel->whenRequestLifecycleIsLongerThan(
+            CarbonInterval::seconds(4),
+            function () {
+                logger()
+                    ->channel('telegram')
+                    ->debug('whenRequestLifecycleIsLongerThan:' . request()->url());
+            }
+        );
     }
 }
