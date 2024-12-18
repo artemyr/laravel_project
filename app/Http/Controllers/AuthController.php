dd943ff7acec31d4c7b5dd306ae1b436d2ab8171
artemyr <artemymaratovitch@yandex.ru>
2024-12-18 11:48:30 +0300

    github авторизация

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,6 +15,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
 
 class AuthController extends Controller
 {
@@ -105,5 +106,29 @@ class AuthController extends Controller
         return $status === Password::PASSWORD_RESET
             ? redirect()->route('login')->with(['message' => __($status)])
             : back()->withErrors(['email' => __($status)]);
+    }
+
+    public function github(): \Symfony\Component\HttpFoundation\RedirectResponse|RedirectResponse
+    {
+        return Socialite::driver('github')
+            ->redirect();
+    }
+
+    public function githubCallback(): RedirectResponse
+    {
+        $githubUser = Socialite::driver('github')->user();
+
+        $user = User::query()->updateOrCreate([
+            'github_id' => $githubUser->id,
+        ], [
+            'name' => $githubUser->name,
+            'email' => $githubUser->email,
+            'password' => bcrypt(str()->random(20)),
+        ]);
+
+        auth()->login($user);
+
+        return redirect()
+            ->intended(route('home'));
     }
 }
