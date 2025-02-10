185d1c2cbf6aeb522a72ceb3c826685eaeae76f4
artemyr <artemymaratovitch@yandex.ru>
2025-02-10 12:01:21 +0300

    тесты

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,12 +5,18 @@ namespace Tests;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Notification;
+use RuntimeException;
 
 abstract class TestCase extends BaseTestCase
 {
     protected function setUp(): void
     {
         parent::setUp();
+
+        if (app()->environment() !== 'testing') {
+            echo "Tests can be run only in testing environment\n";
+            die;
+        }
 
         Notification::fake();
         Http::preventingStrayRequests();
