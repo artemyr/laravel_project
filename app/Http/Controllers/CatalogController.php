b8899713a6037d87a8d88aeae889ce9a55185f47
artemyr <artemymaratovitch@yandex.ru>
2025-06-26 15:33:28 +0300

    view model

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -2,34 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\View\ViewModels\CatalogViewModel;
 use Domain\Catalog\Models\Category;
-use Domain\Product\Models\Product;
-use Illuminate\Contracts\View\Factory;
-use Illuminate\Contracts\View\View;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Foundation\Application;
 
 class CatalogController extends Controller
 {
-    public function __invoke(?Category $category): Factory|View|Application
+    public function __invoke(?Category $category):CatalogViewModel
     {
-        $categories = Category::query()
-            ->select(['id', 'title', 'slug'])
-            ->has('products')
-            ->get();
-
-        $products = Product::query()
-            ->select(['id', 'title', 'slug', 'thumbnail', 'price', 'json_properties'])
-            ->search()
-            ->withCategory($category)
-            ->filtered()
-            ->sorted()
-            ->paginate(6);
-
-        return view('catalog.index', [
-            'category' => $category,
-            'categories' => $categories,
-            'products' => $products
-        ]);
+        return (new CatalogViewModel($category))
+            ->view('catalog.index');
     }
 }
