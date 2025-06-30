96b069d856c56599528f337bb33e52901633a787
artemyr <artemymaratovitch@yandex.ru>
2025-06-30 16:08:27 +0300

    cart

--- app/Providers/DomainServiceProvider.php
+++ app/Providers/DomainServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Domain\Auth\Providers\AuthServiceProvider;
+use Domain\Cart\Providers\CartServiceProvider;
 use Domain\Catalog\Providers\CatalogServiceProvider;
 use Domain\Product\Providers\ProductServiceProvider;
 use Illuminate\Support\ServiceProvider;
@@ -19,6 +20,9 @@ class DomainServiceProvider extends ServiceProvider
         );
         $this->app->register(
             ProductServiceProvider::class
+        );
+        $this->app->register(
+            CartServiceProvider::class
         );
     }
 }
---
+++ src/Domain/Cart/Models/Cart.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Domain\Cart\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Cart extends Model
+{
+    protected $fillable = [
+        'storage_id',
+        'user_id'
+    ];
+
+    public function cartItems(): HasMany
+    {
+        return $this->hasMany(CartItem::class);
+    }
+}
