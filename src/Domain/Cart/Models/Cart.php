4bbbd37bb66a1c6e547ade24dd5083027525525b
artemyr <artem.affetta@gmail.com>
2025-07-14 20:45:19 +0300

    schedule

--- src/Domain/Cart/Models/Cart.php
+++ src/Domain/Cart/Models/Cart.php
@@ -2,11 +2,14 @@
 
 namespace Domain\Cart\Models;
 
+use Illuminate\Database\Eloquent\MassPrunable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cart extends Model
 {
+    use MassPrunable;
+
     protected $fillable = [
         'storage_id',
         'user_id'
@@ -15,5 +18,10 @@ class Cart extends Model
     public function cartItems(): HasMany
     {
         return $this->hasMany(CartItem::class);
+    }
+
+    public function prunable()
+    {
+        return static::where('created_at', '<=', now()->subMonth());
     }
 }
